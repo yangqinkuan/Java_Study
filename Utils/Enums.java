b8239d41f411d0b9012c3298c5bcad43ce9f2b14
yangqinkuan <male_2@163.com>
2018-11-02 15:56:55 +0800

    提交了一个枚举工具

---
+++ Utils/Enums.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+/**
+ * Created by 012466770 ON 2018/11/2.
+ */
+
+public class Enums {
+    private static Random rand = new Random(47);
+    public static <T extends Enum<T>> T random(Class<T> ec){
+        return random(ec.getEnumConstants());
+    }
+    public static <T> T random(T[] values){
+        return values[rand.nextInt(values.length)];
+    }
+}
